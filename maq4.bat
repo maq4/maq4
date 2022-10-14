83914c86655c0bf79dab138691645492bd302343
ZYPH LOVES CATS <111462734+maq4@users.noreply.github.com>
2022-10-14 11:20:17 +0200

    Update maq4.bat

--- maq4.bat
+++ maq4.bat
@@ -1,16 +1,8 @@
 @echo off
-color 02
-echo i love...
-echo i love lori
-:x
-color 04
-i love you
-color 03
-i love you
-color 02
-i love you
-color 01
-i love you
-color 05
-i love you
-goto x
+echo haiiiiii
+pause
+:c
+echo ily sosoososos much!!!
+set /a num=(%Random%%%9)+1
+color %num%
+goto c
